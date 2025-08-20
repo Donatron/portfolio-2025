07539a14436c906c096457ed6dd1ccd7a7600b39
Don Macarthur <donatron@Pacelines-MacBook-Pro.local>
2025-08-20 07:17:17 +0700

    [update-projects] add project-details, project-list and project-title

---
+++ app/components/homepage/projects/project-details.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ProjectList from "./project-list";
+import ProjectTitle from "./project-title";
+
+function ProjectDetails() {
+  return (
+    <>
+      <ProjectTitle />
+      <ProjectList />
+    </>
+  )
+}
+
+export default ProjectDetails
---
+++ app/components/homepage/projects/project-list.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import ProjectCard from "./project-card";
+import { projectsData } from '@/utils/data/projects-data';
+
+function ProjectList() {
+  return (
+    <div className="pt-24">
+      <div className="flex flex-col gap-6">
+        {projectsData.slice(0, 4).map((project, index) => (
+          <div
+            id={`sticky-card-${index + 1}`}
+            key={index}
+            className="sticky-card w-full mx-auto max-w-2xl sticky"
+          >
+            <div className="box-border flex items-center justify-center rounded shadow-[0_0_30px_0_rgba(0,0,0,0.3)] transition-all duration-[0.5s]">
+              <ProjectCard project={project} />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default ProjectList
