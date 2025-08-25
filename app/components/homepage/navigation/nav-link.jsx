585f0f8303ffd63db3d9a2f58a3b49a4dfb3c542
Don Macarthur <donatron@Pacelines-MacBook-Pro.local>
2025-08-25 16:12:55 +0700

    [update-navbar] add smoot scrolling to nav links

--- app/components/homepage/navigation/nav-link.jsx
+++ app/components/homepage/navigation/nav-link.jsx
@@ -4,14 +4,15 @@ import Link from 'next/link'
 function NavLink({ id, href, title }) {
   return (
     <li key={`nav-item-${id}`}>
-      <Link
+      <a
         className="block px-4 py-2 no-underline outline-none hover:no-underline"
         href={`/#${href}`}
+        scroll={false}
       >
         <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
           {title.toUpperCase()}
         </div>
-      </Link>
+      </a>
     </li>
   )
 }
