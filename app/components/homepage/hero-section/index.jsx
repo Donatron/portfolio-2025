4903b4515908ad79374acf21791c46a3a7c1b662
Don Macarthur <donatron@Pacelines-MacBook-Pro.local>
2026-09-03 13:20:14 +1000

    [responsive-layout] improve mobile layout for hero, certifications and achievements

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -20,7 +20,7 @@ function HeroSection() {
       />
 
       <div className="grid grid-cols-1 items-start lg:grid-cols-2 lg:gap-12 gap-y-8">
-        <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
+        <div className="flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
           <HeroTitle />
           <HeroSocialLinks />
           <HeroContactlLinks />
